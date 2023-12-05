7b758c61d5504c06266cfebdd740bf0213075603
Derby <dmatderby@gmail.com>
2023-12-05 14:52:29 +0400

    flower added

--- src/components/ForeText.vue
+++ src/components/ForeText.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
         <hr>
-        <h1 class="main-text m-5">Perspectives on modern marriages</h1>
+        <h1 class="main-text m-5">Perspectives on today's marriages</h1>
+        <FlowerItem />
         <!-- <h5 class="written-by">written by</h5> -->
         <h2 class="writer m-5">CLETUS ELIAKIM MATOMA &copy;</h2>
+        
 
 
     </div>
 </template>
 
 <script>
-    export default {
-        name:'ForeText'
+  import FlowerItem from './FlowerItem.vue';
+
+  export default {
+        name:'ForeText',
+        components: {
+            FlowerItem,
+        }
     }
 </script>
 
