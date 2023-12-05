deacb68c9ac73facab200b39494f6cea8d6c4efc
Derby <dmatderby@gmail.com>
2023-12-05 23:55:42 +0400

    flowe voted down

--- src/components/ForeText.vue
+++ src/components/ForeText.vue
@@ -2,7 +2,7 @@
     <div>
         <hr>
         <h1 class="main-text m-5">Perspectives on today's marriages</h1>
-        <FlowerItem />
+        <!-- <FlowerItem /> -->
         <!-- <h5 class="written-by">written by</h5> -->
         <h2 class="writer m-5">CLETUS ELIAKIM MATOMA </h2>
         
@@ -12,12 +12,12 @@
 </template>
 
 <script>
-  import FlowerItem from './FlowerItem.vue';
+//   import FlowerItem from './FlowerItem.vue';
 
   export default {
         name:'ForeText',
         components: {
-            FlowerItem,
+            // FlowerItem,
         }
     }
 </script>
